e8eb4d211d87ccf20a99298f9c86eb8c258edd9e
Luca Stancapiano <l.stancapiano@sourcesense.com>
2018-08-11 21:16:13 +0200

    rest tests

---
+++ school-ejb/src/main/java/it/vige/school/dto/PupilByDay.java
@@ -0,0 +1,32 @@
+package it.vige.school.dto;
+
+import java.util.Calendar;
+
+public class PupilByDay extends Pupil {
+
+	private static final long serialVersionUID = -2687557693166425255L;
+
+	private Calendar day;
+
+	public PupilByDay() {
+
+	}
+
+	public PupilByDay(Pupil pupil) {
+		setId(pupil.getId());
+		setName(pupil.getName());
+		setPresent(pupil.isPresent());
+		setRoom(pupil.getRoom());
+		setSchool(pupil.getSchool());
+		setSurname(pupil.getSurname());
+	}
+
+	public Calendar getDay() {
+		return day;
+	}
+
+	public void setDay(Calendar day) {
+		this.day = day;
+	}
+
+}
