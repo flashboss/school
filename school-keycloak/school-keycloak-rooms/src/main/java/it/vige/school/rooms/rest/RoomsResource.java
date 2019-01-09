4cc9bb46c6533acd780f08bf5775ca69d91dae1f
Luca Stancapiano <l.stancapiano@sourcesense.com>
2019-01-09 16:43:24 +0100

    JPA Keycloak Provider

--- school-keycloak/school-keycloak-rooms/src/main/java/it/vige/school/rooms/rest/RoomsResource.java
+++ school-keycloak/school-keycloak-rooms/src/main/java/it/vige/school/rooms/rest/RoomsResource.java
@@ -1,10 +1,8 @@
 package it.vige.school.rooms.rest;
 
-import org.jboss.resteasy.annotations.cache.NoCache;
-import org.keycloak.models.KeycloakSession;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
-import it.vige.school.rooms.CompanyRepresentation;
-import it.vige.school.rooms.spi.RoomsService;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -12,41 +10,89 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+
+import org.jboss.resteasy.annotations.cache.NoCache;
+import org.keycloak.models.KeycloakSession;
+
+import it.vige.school.rooms.Room;
+import it.vige.school.rooms.School;
+import it.vige.school.rooms.spi.RoomsService;
 
 public class RoomsResource {
 
 	private final KeycloakSession session;
-	
+
 	public RoomsResource(KeycloakSession session) {
 		this.session = session;
 	}
 
-    @GET
-    @Path("")
-    @NoCache
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<CompanyRepresentation> getCompanies() {
-        return session.getProvider(RoomsService.class).listCompanies();
-    }
+	@GET
+	@Path("")
+	@NoCache
+	@Produces(APPLICATION_JSON)
+	public List<Room> findAllRooms() {
+		return session.getProvider(RoomsService.class).findAllRooms();
+	}
 
-    @POST
-    @Path("")
-    @NoCache
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response createCompany(CompanyRepresentation rep) {
-        session.getProvider(RoomsService.class).addCompany(rep);
-        return Response.created(session.getContext().getUri().getAbsolutePathBuilder().path(rep.getId()).build()).build();
-    }
+	@GET
+	@Path("")
+	@NoCache
+	@Produces(APPLICATION_JSON)
+	public List<School> findAllSchools() {
+		return session.getProvider(RoomsService.class).findAllSchools();
+	}
 
-    @GET
-    @NoCache
-    @Path("{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public CompanyRepresentation getCompany(@PathParam("id") final String id) {
-        return session.getProvider(RoomsService.class).findCompany(id);
-    }
+	@POST
+	@Path("")
+	@NoCache
+	@Consumes(APPLICATION_JSON)
+	public List<Room> findRoomsBySchool(School school) {
+		return session.getProvider(RoomsService.class).findRoomsBySchool(school);
+	}
+
+	@POST
+	@Path("")
+	@NoCache
+	@Consumes(APPLICATION_JSON)
+	public Room createRoom(Room room) {
+		return session.getProvider(RoomsService.class).createRoom(room);
+	}
+
+	@POST
+	@Path("")
+	@NoCache
+	@Consumes(APPLICATION_JSON)
+	public School createSchool(School school) {
+		return session.getProvider(RoomsService.class).createSchool(school);
+	}
+
+	@POST
+	@Path("")
+	@NoCache
+	@Consumes(APPLICATION_JSON)
+	public Response removeRoom(Room room) {
+		session.getProvider(RoomsService.class).removeRoom(room);
+		return Response.created(session.getContext().getUri().getAbsolutePathBuilder()
+				.path(room.getClazz() + "-" + room.getSection() + "-" + room.getSchool()).build()).build();
+	}
+
+	@POST
+	@Path("")
+	@NoCache
+	@Consumes(APPLICATION_JSON)
+	public Response removeSchool(School school) {
+		session.getProvider(RoomsService.class).removeSchool(school);
+		return Response.created(session.getContext().getUri().getAbsolutePathBuilder().path(school.getId()).build())
+				.build();
+	}
+
+	@GET
+	@NoCache
+	@Path("{school}")
+	@Produces(APPLICATION_JSON)
+	public School findSchoolById(@PathParam("school") final String school) {
+		return session.getProvider(RoomsService.class).findSchoolById(school);
+	}
 
 }
